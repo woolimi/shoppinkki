b2cc3700aa47717ec69e44a17cd862db1fdea71b
minssuung <a20751651@gmail.com>
2026-04-09 15:54:59 +0900

    fix: disable collision_monitor in nav2 bringup for real robot

--- src/shoppinkki/shoppinkki_nav/launch/navigation.launch.py
+++ src/shoppinkki/shoppinkki_nav/launch/navigation.launch.py
@@ -52,6 +52,7 @@ def generate_launch_description():
             'map': LaunchConfiguration('map'),
             'params_file': LaunchConfiguration('params_file'),
             'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'use_collision_monitor': 'False',
         }.items(),
     )
 
